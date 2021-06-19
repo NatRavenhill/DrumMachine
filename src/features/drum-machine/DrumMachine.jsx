e7e99a5b75180040cba274bbada8d8a3a98f4020
NatRavenhill <natalie.ravenhill@googlemail.com>
2021-06-19 21:03:25 +0100

    Add styling

--- src/features/drum-machine/DrumMachine.jsx
+++ src/features/drum-machine/DrumMachine.jsx
@@ -4,15 +4,17 @@ import DrumPad from './../drum-pad/DrumPad';
 import { useSelector } from 'react-redux';
 
 const DrumMachine = () => {
-    const drums = useSelector((state) => state.drumMachine.drums);
-    return (
-        <div id="drum-machine">
-          <h1>Drum Machine</h1>
-          <Display />
-           {drums.map(drum => {
-            return <DrumPad key={drum.name} name={drum.name} audio={drum.audio} />
-          })}
-        </div>);
+  const drums = useSelector((state) => state.drumMachine.drums);
+  return (
+    <div id="drum-machine">
+      <h1>Drum Machine</h1>
+      <Display />
+      <div id="drums">
+        {drums.map(drum => {
+          return <DrumPad key={drum.name} name={drum.name} audio={drum.audio} />
+        })}
+      </div>
+    </div>);
 }
 
 export default DrumMachine;
